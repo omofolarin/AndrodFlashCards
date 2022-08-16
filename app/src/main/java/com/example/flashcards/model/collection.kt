4d9f3ef8b2fb3fa5d2a5d327491ac62de5f9fbef
omofolarin <folarinshonibare@yahoo.com>
2022-08-16 03:35:22 +0100

    updated project and added dependenecies

--- app/src/main/java/com/example/flashcards/model/collection.kt
+++ app/src/main/java/com/example/flashcards/model/collection.kt
@@ -1,3 +1,64 @@
 package com.example.flashcards.model
 
-data class collection()
+import androidx.compose.material.Colors
+import androidx.compose.ui.graphics.Color
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MetaData(
+    val createdAt: LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.UTC),
+    val updatedAt: LocalDateTime? = null
+)
+
+@Serializable
+data class Collection(
+    val id: String = "",
+    val title: String = "",
+    val summary: String = "",
+    val metaData: MetaData
+)
+
+
+@Serializable
+data class FlashCard(
+    val id: String,
+    val collectionId: String,
+    val collection: Collection? = null,
+    val frontFace: FlashCardFront,
+    val backFace: FlashCardBack,
+    val color: String = Color.White.toString(),
+    val stats: Stats
+)
+
+@Serializable
+data class FlashCardFront(
+    val question: String,
+    val hint: String?
+)
+
+
+@Serializable
+data class FlashCardBack(
+    val answer: String,
+    val selfAsses: SelfAsses?,
+    val keywords: Set<String> = setOf(),
+)
+
+
+@Serializable
+enum class SelfAsses {
+    DoNotKnowThat,
+    IKnowThat,
+}
+
+@Serializable
+data class Stats(
+    val noOfShuffles: Int,
+    val lastShuffled: LocalDateTime,
+
+)
--- app/src/main/java/com/example/flashcards/ui/collections/Collections.kt
+++ app/src/main/java/com/example/flashcards/ui/collections/Collections.kt
@@ -1,2 +1,37 @@
 package com.example.flashcards.ui.collections
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CollectionCard(label: String, summary: String, isPinned: Boolean) {
+    Card(backgroundColor = Color.White, modifier = Modifier.defaultMinSize(minHeight = 100.dp, minWidth = 80.dp)) {
+       IconButton(onClick = { /*TODO*/ }) {
+        Icon(Icons.Filled.Star, contentDescription = "is pinned", modifier = Modifier.size(20.dp))
+
+       }
+
+        Column(Modifier.padding(4.dp)) {
+            Text(text = label)
+            Text(text = summary, fontSize =8.sp)
+        }
+
+    }
+}
+
+@Preview(name="Flashcard collection")
+@Composable
+private  fun CollectionCardPreview() {
+    CollectionCard(label = "Collection name", summary = "", isPinned = false)
+}
